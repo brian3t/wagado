15f13d848f5b8a3e5fc888d0ba8a229a13035613
brian3t <ngxtri@gmail.com>
2020-11-01 10:57:46 -0800

    WIP lucid query builder game.findOrNew

--- app/Commands/ScrEspnGame.js
+++ app/Commands/ScrEspnGame.js
@@ -25,30 +25,88 @@ class ScrEspnGame extends Command {
       if (! nfl_scheds || nfl_scheds.status !== 200) return
       const $ = await cheerio.load(nfl_scheds.data)
       $('h2.table-caption').each(async (i, game_date) => {
+        let game_date_db = null
         try {
           const $game_date = await $(game_date)
-          let game_date_text = $game_date.text() //Thursday, October 29
-          let game_date_mm = mm(game_date_text, '')
+          let game_date_text = await $game_date.text() //Thursday, October 29
+          let game_date_mm = mm(game_date_text, 'dddd, MMMM DD')
+          if (! game_date_mm.isValid()) {
+            return
+          }
+          game_date_mm.hour(8)
+          game_date_db = game_date_mm.format('YYYY-MM-DD')
 
-          const name = await $game_date.find('h4').text().trim()
-          let nfl_team_id = await $game_date.find('a[data-link_name="1st CTA View Profile"]').attr('href') // teams/arizona-cardinals/
-          nfl_team_id = nfl_team_id.replace('teams/', '').replace(/\//g, '')
-          let official_website = await $game_date.find('a[data-link_name="2nd CTA View Full Site"]').attr('href') // teams/arizona-cardinals/
-          let logo_nflcom = await $game_date.find('figure.d3-o-media-object__figure source[media="(min-width:1024px)"]').attr('srcset')
-          let team = new Team()
-          team.name = name
-          team.nfl_team_id = nfl_team_id
-          team.official_website = official_website
-          team.logo_nflcom = logo_nflcom
-          console.log(`saving team now`)
-          await team.save()
-          let a = 1
+          //now get sibling div containing list of games
+          let div_list_of_games = await $game_date.siblings('div.responsive-table-wrap')
+          if (typeof div_list_of_games !== 'object' || ! (div_list_of_games[0])) return
+          div_list_of_games = await $(div_list_of_games[0]) //my immediate next sibling
+
+          await div_list_of_games.find('tbody > tr').each(async (i, game_tr) => {//for each game row
+            let $game_tr = await $(game_tr)
+            let $game_tds = await $($game_tr.find('td'))
+            if (! $game_tds || ! $game_tds[0]) return
+
+            let $first_td = $($game_tds[0]) //first td
+            let $a_team_name = await $($first_td.find('a.team-name'))
+            if ($a_team_name.length !== 1) return
+            let a_team_name_abbr = await $a_team_name.find('abbr').attr('title')
+            let away_team_found = await Team.findBy('name', a_team_name_abbr)
+            if (! away_team_found instanceof Team) return
+
+            if (! $game_tds[1]) return
+            let $second_td = $($game_tds[1]) //second td
+            $a_team_name = await $($second_td.find('a.team-name'))
+            if ($a_team_name.length !== 1) return
+            a_team_name_abbr = await $a_team_name.find('abbr').attr('title')
+            let home_team_found = await Team.findBy('name', a_team_name_abbr)
+            if (! home_team_found instanceof Team) return
+
+            let exist_or_new_game = Game.findOrNew(`teamh_id = ${home_team_found.id} AND teama_id = ${away_team_found.id} AND game_date = ${game_date_db}`,
+              {
+                teamh_id: home_team_found.id, teama_id: away_team_found.id, game_date: game_date_db
+              })
+
+            if (! $game_tds[2]) return
+            let $third_td = $($game_tds[2]) //third td
+            if ($third_td.attr('data-date')) { //upcoming game
+              let game_date_time = mm($third_td.attr('data-date').replace('Z', ''), 'YYYY-MM-DDTHH:mm') //2020-11-01T21:05Z
+              if (! game_date_time.isValid()) return
+            } else {
+              let a_result = $third_td.find('a[name="&lpos=nfl:schedule:score"]')
+              if (a_result.length === 1) {
+                a_result = a_result.text() // ATL 25, CAR 17
+                let all_teams_all_points = [] //store all teams' points
+                let points = a_result.split(',')
+                if (points.length === 2) {
+                  let point_1 = points[0].split(' ')
+                  if (point_1.length === 2) {
+                    all_teams_all_points.push(point_1)
+                  }
+                  let point_2 = points[0].split(' ')
+                  if (point_2.length === 2) {
+                    all_teams_all_points.push(point_2)
+                  }
+                }
+                all_teams_all_points.forEach((team_point) => {
+                  let team_abbr = team_point[0], point = team_point[1] //ATL 25
+
+                })
+              }
+              let a = 1
+
+            }
+            $a_team_name = await $($second_td.find('a.team-name'))
+
+            let a = 1
+
+
+          })
         } catch (e) {
-          console.error(`Error 44: `, e)
+          console.error(`Error 57: `, e)
         }
       })
     } catch (e) {
-      console.error(`error 48: `, e)
+      console.error(`error 61: `, e)
     }
   }
 }
